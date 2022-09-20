17f4255ac16dc652776eef08ee84f3da90ffbe3a
anurag629 <akvermaav629@gmail.com>
2022-09-20 22:39:57 +0530

    tested

--- carousel/models.py
+++ carousel/models.py
@@ -5,13 +5,13 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class Carousel(models.Model):
     added_at = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100, default='')
-    photo = models.ImageField(upload_to='media/carousal')
+    photo = models.ImageField(upload_to='carousal')
     is_active = models.BooleanField(default=False)
     preference = models.IntegerField(unique=True,
-                                     validators=[MaxValueValidator(100), MinValueValidator(1)])
+                                     validators=[MaxValueValidator(6), MinValueValidator(1)])
 
     class Meta:
         ordering = ['added_at']
 
-    def __init__(self):
+    def __str__(self):
         return self.title
