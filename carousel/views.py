c257adce1f1a8f6f1a2ead83ad47f79118fde3c9
anurag629 <akvermaav629@gmail.com>
2022-09-22 00:20:34 +0530

    updated JSONResponce to Response

--- carousel/views.py
+++ carousel/views.py
@@ -1,11 +1,11 @@
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from carousel.models import Carousel
 from carousel.serializers import CarouselSerializer
 
 
-@csrf_exempt
+@api_view(['GET', 'POST'])
 def carousel_list(request):
     """
     List all carousel, or create a new carousal.
@@ -13,18 +13,17 @@ def carousel_list(request):
     if request.method == 'GET':
         carousels = Carousel.objects.all()
         serializer = CarouselSerializer(carousels, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = CarouselSerializer(data=data)
+        serializer = CarouselSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@csrf_exempt
+@api_view(['GET', 'PUT', 'DELETE'])
 def carousel_detail(request, pk):
     """
     Retrieve, update or delete a carousel.
@@ -32,20 +31,19 @@ def carousel_detail(request, pk):
     try:
         carousel = Carousel.objects.get(pk=pk)
     except Carousel.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = CarouselSerializer(carousel)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = CarouselSerializer(carousel, data=data)
+        serializer = CarouselSerializer(carousel, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         carousel.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
