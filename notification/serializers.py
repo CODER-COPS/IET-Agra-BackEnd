ee54403ee2e5bd6dc3a1fed118773f500b884dc7
anurag629 <akvermaav629@gmail.com>
2022-10-05 22:19:33 +0530

    refactor all apps

--- notification/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from notification.models import Notification
-
-
-class NotificationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Notification
-        fields = ['id', 'title', 'description', 'is_active']
