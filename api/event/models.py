8b16dc52b1e8fa8a3b3b7fd4439b928dfe21a7de
Abhishek Gupta <ravi37463@gmail.com>
2022-10-06 21:22:00 +0530

    fix/event

--- api/event/models.py
+++ api/event/models.py
@@ -6,3 +6,10 @@ class Event(models.Model):
     added_at = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100, default = '')
     is_active = models.BooleanField(default = False)
+    
+    
+    class Meta:
+        ordering = ['added_at']
+
+    def __str__(self):
+        return self.title
